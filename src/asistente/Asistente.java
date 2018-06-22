f9b615c6e64dde7d14758e0b8929f3cdbcc32a3d
Brian Servis <brian_servis@yahoo.com>
2018-06-22 07:50:47 -0300

    Se completo la funcion memes
    Se agregaron las fotos de los memes. Se corrigieron las expresiones regulares para detectar  el pedido de memes y de leyes de la robotica.

---
+++ src/asistente/Asistente.java
@@ -0,0 +1,91 @@
+package asistente;
+
+import atencion.Agradecimiento;
+import atencion.Atencion;
+import atencion.Calculo;
+import atencion.CambioDeUnidades;
+import atencion.ChuckNorris;
+import atencion.Clima;
+import atencion.DatoTemporario;
+import atencion.Imagen9Gag;
+import atencion.ImagenMeme;
+import atencion.Juego;
+import atencion.LeyesRobotica;
+import atencion.Saludo;
+import atencion.SinSentido;
+import imagen.ClienteGUI;
+
+import java.text.Normalizer;
+
+public class Asistente {
+
+	private String nombre;
+	private Atencion[] atenciones;
+	private ClienteGUI ventana;
+
+	public final static String USUARIO = "@delucas";
+
+	public Asistente(String nombre) {
+		this.nombre = nombre;
+		this.ventana = new ClienteGUI("Test");
+		asignarCadenaDeAtencion();
+	}
+	
+	public void asignarCadenaDeAtencion() {
+		atenciones = new Atencion[12];
+		atenciones[0] = new Saludo();
+		atenciones[1] = new Agradecimiento();
+		atenciones[2] = new Calculo();
+		atenciones[3] = new Juego();
+		atenciones[4] = new CambioDeUnidades();
+		atenciones[5] = new LeyesRobotica();
+		atenciones[6] = new ChuckNorris();
+		atenciones[7] = new ImagenMeme();
+		atenciones[8] = new Clima();
+		atenciones[9] = new DatoTemporario();
+		atenciones[10] = new Imagen9Gag();
+		atenciones[11] = new SinSentido();
+		
+		atenciones[0].establecerSiguiente(atenciones[1]);
+		atenciones[1].establecerSiguiente(atenciones[2]);
+		atenciones[2].establecerSiguiente(atenciones[3]);
+		atenciones[3].establecerSiguiente(atenciones[4]);
+		atenciones[4].establecerSiguiente(atenciones[5]);
+		atenciones[5].establecerSiguiente(atenciones[6]);
+		atenciones[6].establecerSiguiente(atenciones[7]);
+		atenciones[7].establecerSiguiente(atenciones[8]);
+		atenciones[8].establecerSiguiente(atenciones[9]);
+		atenciones[9].establecerSiguiente(atenciones[10]);
+		atenciones[10].establecerSiguiente(atenciones[11]);
+
+	}
+
+	public String escuchar(String mensaje) {
+		mensaje = mensaje.toUpperCase();
+		mensaje = Normalizer.normalize(mensaje, Normalizer.Form.NFD);
+		mensaje = mensaje.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+		
+		return atenciones[0].atender(mensaje, nombre, USUARIO);
+	}
+	
+	public static void main(String[] args) {
+		Asistente asistente = new Asistente("jenkins");
+		String respuesta = asistente.escuchar("@jenkins decime una frase de Chuck Norris.");
+		System.out.println(respuesta);
+		
+		respuesta = asistente.escuchar("@jenkins take my money");
+		asistente.ventana.imprimirImagen(respuesta);
+		//System.out.println(respuesta);
+		
+		respuesta = asistente.escuchar("@jenkins quiero saber el clima de San Justo");
+		asistente.ventana.imprimir(respuesta);
+		
+		respuesta = asistente.escuchar("@jenkins quiero una imagen de 9gag");
+		String[] respuestas = respuesta.split("\\|");
+		asistente.ventana.imprimirImagen(respuestas[1]);
+		asistente.ventana.imprimir(respuestas[0]);
+		
+	}
+}
+
+
